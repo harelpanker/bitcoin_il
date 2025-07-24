e309c6c2afdc0c57ced2cdb2079f0acb4ba34bad
Harel Panker <harelpanker@gmail.com>
2025-07-24 20:23:00 +0200

    first commit

---
+++ components/ui/section.tsx
@@ -0,0 +1,16 @@
+import clsx from 'clsx';
+
+type SectionProps = {
+	element?: 'section' | 'header' | 'main';
+} & React.ComponentPropsWithoutRef<'section' | 'header' | 'main'>;
+
+export default function Section({ className, element = 'section', children, ...props }: SectionProps) {
+	const Element = element;
+	return (
+		<Element
+			{...props}
+			className={clsx(className, `relative z-20 flex flex-col ${element !== 'main' ? ' px-5 lg:px-12' : 'grow'}`)}>
+			{children}
+		</Element>
+	);
+}
