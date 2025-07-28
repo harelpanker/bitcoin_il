9c8270960c2226ca1963d46dd3df7eece6090e4a
Harel Panker <harelpanker@gmail.com>
2025-07-28 14:09:24 +0200

    gradient animation

--- components/ui/span-gradient.tsx
+++ components/ui/span-gradient.tsx
@@ -1,6 +1,9 @@
+import styles from './css-gradient.module.css';
+
 export default function SpanGradient({ children }: { children: React.ReactNode }) {
 	return (
-		<span className='to-primary inline-block bg-gradient-to-br from-orange-400 bg-clip-text text-transparent'>
+		<span
+			className={`${styles.animateGradient} to-primary inline-block bg-gradient-to-br from-orange-400 bg-clip-text text-transparent`}>
 			{children}
 		</span>
 	);
