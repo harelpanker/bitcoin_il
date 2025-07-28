e59153a6c7f453d9ef0dd2bb2df4d30d7842f6ac
Harel Panker <harelpanker@gmail.com>
2025-07-28 10:35:37 +0200

    Adding loader

--- components/analytics-page/price-over-time.tsx
+++ components/analytics-page/price-over-time.tsx
@@ -5,6 +5,7 @@ import { Area, AreaChart, CartesianGrid, XAxis, YAxis } from 'recharts';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart';
 import { BitcoinPriceData, HistoricalDataResponse } from '@/lib/types/price-over-time';
+import { Skeleton } from '@/components/ui/skeleton';
 import { genericFetcher } from '@/lib/generic-fetcher';
 import { formatUSD } from '@/lib/formater';
 
@@ -19,8 +20,9 @@ export default function AnalyticsClientPage() {
 		isLoading: priceLoading,
 	} = useSWR<BitcoinPriceData>('/api/bitcoin/bitcoin-price', priceFetcher);
 
-	if (priceLoading) {
-		return <div className='py-4 text-center'>Loading Bitcoin price...</div>;
+	if (error) {
+		console.error('Error fetching historical Bitcoin price:', error);
+		return <div className='py-4 text-center text-orange-500'>Failed to load historical data: {error.message}</div>;
 	}
 
 	if (priceError) {
@@ -28,25 +30,8 @@ export default function AnalyticsClientPage() {
 		return <div className='py-4 text-center text-orange-500'>Failed to load Bitcoin price: {priceError.message}</div>;
 	}
 
-	if (!priceData || typeof priceData.price !== 'number') {
-		return <div className='py-4 text-center text-orange-500'>No Bitcoin price data available.</div>;
-	}
-
-	if (isLoading) {
-		return <div className='py-4 text-center'>Loading Bitcoin historical price data...</div>;
-	}
-
-	if (error) {
-		console.error('Error fetching historical Bitcoin price:', error);
-		return <div className='py-4 text-center text-orange-500'>Failed to load historical data: {error.message}</div>;
-	}
-
-	if (!data?.historicalPrices || data.historicalPrices.length === 0) {
-		return <div className='py-4 text-center text-orange-500'>No historical Bitcoin price data available.</div>;
-	}
-
-	const historicalData = data.historicalPrices;
-	const bitcoinPrice = priceData.price;
+	const historicalData = data?.historicalPrices;
+	const bitcoinPrice = priceData?.price;
 
 	const chartConfig = {
 		price: {
@@ -59,58 +44,66 @@ export default function AnalyticsClientPage() {
 		<Card className='col-span-1 md:col-span-4'>
 			<CardHeader>
 				<CardTitle>Current Bitcoin Price</CardTitle>
-				<CardDescription>
-					<span className='font-semibold'>
-						{bitcoinPrice.toLocaleString('en-US', { style: 'currency', currency: 'USD' })} USD
-					</span>
-				</CardDescription>
+				{!priceLoading && bitcoinPrice ? (
+					<CardDescription>
+						<span className='font-semibold'>
+							{bitcoinPrice.toLocaleString('en-US', { style: 'currency', currency: 'USD' })} USD
+						</span>
+					</CardDescription>
+				) : (
+					<Skeleton className='h-4 w-28 rounded-full'></Skeleton>
+				)}
 			</CardHeader>
-			<CardContent>
-				<ChartContainer config={chartConfig}>
-					<AreaChart
-						accessibilityLayer
-						data={historicalData}
-						margin={{
-							left: 12,
-							right: 12,
-						}}>
-						<CartesianGrid vertical={false} />
+			<CardContent className='aspect-[311/125] w-full lg:aspect-[960/384]'>
+				{!isLoading && historicalData ? (
+					<ChartContainer config={chartConfig}>
+						<AreaChart
+							accessibilityLayer
+							data={historicalData}
+							margin={{
+								left: 12,
+								right: 12,
+							}}>
+							<CartesianGrid vertical={false} />
 
-						<XAxis
-							dataKey='date'
-							tickLine={false}
-							axisLine={false}
-							tickMargin={8}
-							minTickGap={32}
-							tickFormatter={(value) => value.slice(0, 6)}
-						/>
+							<XAxis
+								dataKey='date'
+								tickLine={false}
+								axisLine={false}
+								tickMargin={8}
+								minTickGap={32}
+								tickFormatter={(value) => value.slice(0, 6)}
+							/>
 
-						<YAxis
-							tickLine={false}
-							axisLine={false}
-							tickMargin={8}
-							tickCount={6}
-							tickFormatter={(value) => formatUSD(value)}
-						/>
+							<YAxis
+								tickLine={false}
+								axisLine={false}
+								tickMargin={8}
+								tickCount={6}
+								tickFormatter={(value) => formatUSD(value)}
+							/>
 
-						<ChartTooltip cursor={true} content={<ChartTooltipContent />} />
+							<ChartTooltip cursor={true} content={<ChartTooltipContent />} />
 
-						<defs>
-							<linearGradient id='fillPrice' x1='0' y1='0' x2='0' y2='1'>
-								<stop offset='5%' stopColor='var(--chart-3)' stopOpacity={0.8} />
-								<stop offset='95%' stopColor='var(--chart-3)' stopOpacity={0.1} />
-							</linearGradient>
-						</defs>
-						<Area
-							dataKey='price'
-							type='basisOpen'
-							fill='url(#fillPrice)'
-							fillOpacity={0.4}
-							stroke='var(--chart-3)'
-							stackId='a'
-						/>
-					</AreaChart>
-				</ChartContainer>
+							<defs>
+								<linearGradient id='fillPrice' x1='0' y1='0' x2='0' y2='1'>
+									<stop offset='5%' stopColor='var(--chart-3)' stopOpacity={0.8} />
+									<stop offset='95%' stopColor='var(--chart-3)' stopOpacity={0.1} />
+								</linearGradient>
+							</defs>
+							<Area
+								dataKey='price'
+								type='basisOpen'
+								fill='url(#fillPrice)'
+								fillOpacity={0.4}
+								stroke='var(--chart-3)'
+								stackId='a'
+							/>
+						</AreaChart>
+					</ChartContainer>
+				) : (
+					<Skeleton className='aspect-[311/125] w-full rounded-2xl lg:aspect-[960/384]'></Skeleton>
+				)}
 			</CardContent>
 		</Card>
 	);
